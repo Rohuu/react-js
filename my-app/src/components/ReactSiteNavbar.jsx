7d22125b912bff1f7c8fd3ecb9dc824ba6825c8c
Rohit Singh Bhandari <rohuu99022@gmail.com>
2023-08-24 23:46:13 +0530

    added more styling

--- my-app/src/components/ReactSiteNavbar.jsx
+++ my-app/src/components/ReactSiteNavbar.jsx
@@ -3,11 +3,9 @@ import React from "react";
 function ReactSiteNavbar(){
     return(
         <nav className="nav-items"> 
-            <div>
-                <img src="../../public/logo192.png"/>
-                <h3>ReactFacts</h3>
-                <h4>React Course - Project 1 </h4>
-            </div>
+                <img src="../public/logo192.png"/>
+                <h3 className="nav-logo-text">ReactFacts</h3>
+                <h4 className="nav-title">React Course - Project 1 </h4>
         </nav>
     )
 }
