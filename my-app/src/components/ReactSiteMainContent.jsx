e1551016414710d4f91aca0563fce77db31ca6a3
Rohit Singh Bhandari <rohuu99022@gmail.com>
2023-09-04 23:11:13 +0530

    completed styling

--- my-app/src/components/ReactSiteMainContent.jsx
+++ my-app/src/components/ReactSiteMainContent.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function ReactSiteMainContent(){
     return(
         <div className="container"> 
-            <h1>Fun facts about React</h1>
+            <h1 className="main-title">Fun facts about React that everyone should know!!</h1>
             <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100k </li>
-                <li>Is maintained by Facebook</li>
-                <li>power thousands of apps including mobile apps</li>
+                <li>Was first released in 2013 and you should learn it. Has well over 100k, you should learn it.</li>
+                <li>Was originally created by Jordan Walke, you should learn it.Has well over 100k.</li>
+                <li>Has well over 100k, you should learn it. Has well over 100k, you should learn it.</li>
+                <li>Is maintained by Facebook, you should learn it. Has well over 100k, you should learn it.</li>
+                <li>Powers thousands of apps including mobile apps, you should learn it. Has well over 100k, you should learn it.</li>
             </ul>
         </div>
     )
--- my-app/src/components/ReactSiteNavbar.jsx
+++ my-app/src/components/ReactSiteNavbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import logoImage from "../images/logo192.png";
 
 function ReactSiteNavbar(){
     return(
         <nav className="nav-items"> 
-                <img src="../public/logo192.png"/>
+                <img src={logoImage} className="nav-icon"/>
                 <h3 className="nav-logo-text">ReactFacts</h3>
                 <h4 className="nav-title">React Course - Project 1 </h4>
         </nav>
